78276ee9e4a8b6de4b6b65db7f9c4fe6c646d17a
kasee18 <mutisyavincent99@gmail.com>
2025-07-16 16:24:11 +0300

    added work

---
+++ Intro.py
@@ -0,0 +1,5 @@
+#print is a method used to display an output
+print("Triple7")
+print("I am slim")
+print(100)
+print(5 + 6)
---
+++ variables.py
@@ -0,0 +1,15 @@
+number = 10
+num2 = 6
+first_name = "John" #snare case
+lastName = "Doe" #Pascal
+language = "Python"
+
+print(number)
+print(num2)
+print(first_name)
+print(lastName)
+print(language)
+print(number + num2)
+print("My first name is ", first_name)
+print("My full name is ", first_name, lastName)
+print(language, "is a web programming language")
